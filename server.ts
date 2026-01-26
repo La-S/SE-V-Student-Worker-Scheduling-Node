7f14e64cf4705455a57b64fec2b2d5c71e3862e4
Lance Skinner <sirlancelot2604@outlook.com>
2026-01-26 15:15:50 -0600

    change base url

--- server.ts
+++ server.ts
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
   
 // Load the routes from the routes folder
-app.use("/tracker-t3", routes); 
+app.use("/workerscheduling-t3", routes); 
 
 // set port, listen for requests
 const PORT = process.env.PORT || 3100;
