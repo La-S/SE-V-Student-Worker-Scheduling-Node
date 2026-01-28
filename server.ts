55ae15bd9b7f24a3f6558203e91f065632f2669d
Lance Skinner <sirlancelot2604@outlook.com>
2026-01-28 13:00:19 -0600

    add insecure http for dev

--- server.ts
+++ server.ts
@@ -9,13 +9,23 @@ import fs from 'fs';
 
 const app = express();
 
+<<<<<<< Updated upstream
 // Also use the cors middleware as backup
 var corsOptions = {
   origin: "https://localhost:8081",
   credentials: true
-}
-app.use(cors(corsOptions));
+=======
+const fbApp = firebaseApp;
 
+if (process.env.NODE_ENV == "dev") {
+  // add dev cors options.
+  var cors2Options = {
+    origin: ["http://localhost:8081",  "https://localhost:8081"],
+    credentials: true
+  }
+  app.use(cors(cors2Options));
+>>>>>>> Stashed changes
+}
 
 // parse requests of content-type - application/json
 app.use(express.json());
@@ -27,7 +37,7 @@ app.use("/workerscheduling-t3", routes);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 3100;
-if (process.env.NODE_ENV == "dev") {
+if (process.env.NODE_ENV == "dev" && !process.env.USE_HTTP) {
   https.createServer({
     key: fs.readFileSync('./localhost+2-key.pem'),
     cert: fs.readFileSync('./localhost+2.pem'),
