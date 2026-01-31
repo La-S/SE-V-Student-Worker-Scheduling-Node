f9aa9fcd8267bd20a40f0cc0a16d15b86aeb075c
JohnEveryOC <j.every@eagles.oc.edu>
2026-01-31 16:14:54 -0600

    refactor to try await catch

--- app/controllers/businessunit.controller.ts
+++ app/controllers/businessunit.controller.ts
@@ -15,121 +15,103 @@ exports.create = async (req: pkg.Request, res: pkg.Response) => {
   };
 
   // Save User in the database
-  BusinessUnit.create(businessUnit as any)
-    .then((data: any) => {
-      res.send(data);
-    })
-    .catch((err: any) => {
-      if (err.name === 'SequelizeValidationError') {
-        res.status(400).send({
-          message: err.message
-        })
-        return;
-      }
-      res.status(500).send({
-        message: err.message || "Some error occurred while creating the Business Unit.",
-      });
+  try {
+    const data = await BusinessUnit.create(businessUnit as any)
+    res.send(data);
+  }
+  catch (err: any) {
+    res.status(500).send({
+      message: err.message || "Some error occurred while creating the Business Unit.",
     });
+  }
 };
 
 // Retrieve all BusinessUnits from the database.
-exports.findAll = (req: pkg.Request, res: pkg.Response) => {
+exports.findAll = async (req: pkg.Request, res: pkg.Response) => {
 
-  BusinessUnit.findAll()
-    .then((data: any) => {
-      res.send(data);
-    })
-    .catch((err: any) => {
-      res.status(500).send({
-        message: err.message || "Some error occurred while retrieving all business units.",
-      });
+  try {
+    const data = await BusinessUnit.findAll()
+    res.send(data);
+  }
+  catch (err: any) {
+    res.status(500).send({
+      message: err.message || "Some error occurred while getting all Business Units.",
     });
+  }
 };
 
 // Find a single User with an id
-exports.findOne = (req: pkg.Request, res: pkg.Response) => {
+exports.findOne = async (req: pkg.Request, res: pkg.Response) => {
   const id = req.params.id;
 
-  BusinessUnit.findByPk(id)
-    .then((data: any) => {
-      if (data) {
-        res.send(data);
-      } else {
-        res.status(404).send({
-          message: `Cannot find Business Unit with id=${id}.`,
-        });
-      }
-    })
-    .catch((err: any) => {
-      res.status(500).send({
-        message: "Error retrieving Business Unit with id=" + id,
-      });
+  try {
+    const data = await BusinessUnit.findByPk(id);
+    if (!data) {
+      res.status(404).send({
+        message: `Business Unit for id ${id} not found`
+      })
+      return;
+    }
+    res.send(data);
+  }
+  catch (err: any) {
+    res.status(500).send({
+      message: err.message || "Some error occurred.",
     });
+  }
 };
 
 // Update a BusinessUnit by the id in the request
 exports.update = async (req: pkg.Request, res: pkg.Response) => {
   const id = parseInt(req.params.id, 10);
-  let businessUnitForId = await BusinessUnit.findByPk(id);
-  if (!businessUnitForId) {
-    res.status(404).send({ message: `Business Unit for id ${id} not found.` });
-    return;
-  }
-  BusinessUnit.update(req.body, {
-    where: { id: id },
-  })
-    .then((num: [number]) => {
-      if (num[0] == 1) {
-        res.send({
-          message: "Business Unit was updated successfully.",
-        });
-      } else {
-        res.status(400).send({
-          message: `Cannot update Business Unit with id=${id}. Please check request body!`,
-        });
-      }
-    })
-    .catch((err: any) => {
-      if (err.name === 'SequelizeValidationError') {
-        res.status(400).send({
-          message: err.message
-        });
-        return;
-      }
-      res.status(500).send({
-        message: "Error updating Business Unit with id=" + id,
-      });
+  try {
+    let businessUnitForId = await BusinessUnit.findByPk(id);
+    if (!businessUnitForId) {
+      res.status(404).send({ message: `Business Unit for id ${id} not found.` });
+      return;
+    }
+    const numChanged = await BusinessUnit.update(req.body, {
+      where: { id: id },
     });
+    if (numChanged[0] <= 0) {
+      res.status(400).send({ message: `Update for id ${id} did not update. Check request body.` })
+      return;
+    }
+    let updatedBusinessUnit = await BusinessUnit.findByPk(id);
+    res.send(updatedBusinessUnit)
+  }
+  catch (err: any) {
+    res.status(500).send({
+      message: err.message || "Some error occurred.",
+    });
+  }
 };
 
 // Delete a Business Unit with the specified id in the request
 exports.delete = async (req: pkg.Request, res: pkg.Response) => {
   const id = parseInt(req.params.id, 10);
-  let businessUnitForId = await BusinessUnit.findByPk(id);
-  if (!businessUnitForId) {
-    res.status(404).send({ message: `Business Unit for id ${id} not found.` });
-    return;
+  try {
+    let businessUnitForId = await BusinessUnit.findByPk(id);
+    if (!businessUnitForId) {
+      res.status(404).send({ message: `Business Unit for id ${id} not found.` });
+      return;
+    }
+
+    const numDeleted = await BusinessUnit.destroy({
+      where: { id: id },
+    })
+    if (numDeleted <= 0) {
+      res.status(400).send({ message: `Delete for id ${id} did not delete. Check request body.` })
+      return;
+    }
+    res.status(200).send({ message: "Business Unit deleted successfully!" });
+  }
+  catch (err: any) {
+    res.status(500).send({
+      message: err.message || "Some error occurred.",
+    });
   }
 
-  BusinessUnit.destroy({
-    where: { id: id },
-  })
-    .then((num: number) => {
-      if (num == 1) {
-        res.send({
-          message: "Business Unit was deleted successfully!",
-        });
-      } else {
-        res.send({
-          message: `Cannot delete Business Unit with id=${id}. Maybe User was not found!`,
-        });
-      }
-    })
-    .catch((err: string) => {
-      res.status(500).send({
-        message: "Could not delete Business Unit with id=" + id,
-      });
-    });
 };
 
 
