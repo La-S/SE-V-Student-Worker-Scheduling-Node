60406efba92949cd9a3130b6e9b963a1e305bdb1
JohnEveryOC <j.every@eagles.oc.edu>
2026-01-30 21:19:05 -0600

    fix open routes

--- app/routes/index.ts
+++ app/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 
 import AuthRoutes from "./auth.routes.js";
 import UserRoutes from "./user.routes.js";
+import BusinessUnitRoutes from "./businessunit.routes.js";
 
 const router = Router();
 
 router.use("/", AuthRoutes);
 router.use("/users", UserRoutes);
+router.use("/businessunits", BusinessUnitRoutes);
 
 export default router;
