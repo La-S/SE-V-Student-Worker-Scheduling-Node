8f1cce97ab4074043c87f39fdc5d1f6424ea0d70
JohnEveryOC <j.every@eagles.oc.edu>
2026-02-04 14:31:31 -0600

    fix auth error reporting and style consistency

--- app/authorization/authorization.ts
+++ app/authorization/authorization.ts
@@ -5,39 +5,43 @@ import type { SessionType } from "../types/session.type.ts";
 const Session = db.Session;
 
 const auth: any = {};
-auth.authenticate = (req: pkg.Request, res: pkg.Response, next: pkg.NextFunction) => {
+auth.authenticate = async (req: pkg.Request, res: pkg.Response, next: pkg.NextFunction) => {
   let token = null;
 
   let authHeader = req.get("authorization");
-  if (authHeader != null) {
-    if (authHeader.startsWith("Bearer ")) {
-      token = authHeader.slice(7);
-
-      Session.findAll({ where: { token: token } })
-        .then((data) => {
-          let session = data[0].dataValues as SessionType;
-          console.log(session.expirationDate);
-          if (session != null) {
-            if (session.expirationDate.getTime() >= Date.now()) {
-              next();
-              return;
-            } else
-              return res.status(401).send({
-                message: "Unauthorized! Expired Token, Logout and Login again",
-              });
-          }
-        })
-        .catch((err) => {
-          return res.status(500).send({
-            message: err.message || "an unknown error occurred while authenticating",
-          });
-        });
-    }
-  } else {
+  if (authHeader == null) {
     return res.status(401).send({
-      message: "Unauthorized! No Auth Header",
+      message: "Unauthorized! No Auth header!",
     });
   }
+  if (!authHeader.startsWith("Bearer ")) {
+    return res.status(401).send({
+      message: "Unauthorized! Please Use Bearer Token for authentication!",
+    });
+  }
+  token = authHeader.slice(7);
+  try {
+    const sessions = await Session.findAll({ where: { token: token } })
+    if (sessions.length == 0) {
+      return res.status(500).send({ message: "No sessions found for user" });
+    }
+    let session = sessions[0].dataValues as SessionType;
+    console.log(session.expirationDate);
+    if (session != null) {
+      if (session.expirationDate.getTime() >= Date.now()) {
+        next();
+        return;
+      } else
+        return res.status(401).send({
+          message: "Unauthorized! Expired Token, Logout and Login again",
+        });
+    }
+  }
+  catch (err : any) {
+    return res.status(500).send({
+      message: err.message || "an unknown error occurred while authenticating",
+    });
+  };
 };
 
 // //AUTHORIZATION METHOD, DOES NOT REPLACE AUTHENTICATE
