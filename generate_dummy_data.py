3da5d5c101d91665c3caffc917c0d2dee44f0ca0
Lance Skinner <sirlancelot2604@outlook.com>
2026-02-17 11:36:38 -0600

    add your user, tasks, and tasklists

--- generate_dummy_data.py
+++ generate_dummy_data.py
@@ -13,13 +13,25 @@ ADMIN_KEY = "admin"
 CLEANUP_OLD_ARTIFACTS = True # if True, replaces all old users with new ones.
 WIPE_DB = False # UNDER DEVELOPMENT (doesn't work): if True, fully resets the DB (expect sessions which allow this to work?) before putting this new data.
 SECRET_PASSWORD = os.getenv("SECRET_PASSWORD")
+YOUR_EMAIL = "l.skinner@eagles.oc.edu"
 
 users_generated = 0
 business_units_generated = 0
 employees_generated = 0
 positions_created = 0
 shifts_generated = 0
+tasklists_generated = 0
+tasks_generated = 0
 
+
+def get_existing_user(email):
+    r = requests.get(f'{ENDPOINT}/debug/bdiohjaiofjas/user/email', data = {
+        "email": email,
+        "password": SECRET_PASSWORD,
+    }, verify=False)
+    if r.status_code != 200:
+        print("Error, couldn't get existing user...")
+    return r.json()
 
 def create_user(first_name, last_name, email, isAdmin):
     global users_generated
@@ -128,6 +140,36 @@ def create_shift(employeeId, businessUnitId, positionId, startTime, endTime, dat
         shifts_generated += 1
     else:
         print('Hmm, we got an error creating a shift', r.text)
+        print(startTime, endTime)
+
+    return r.json()
+
+def create_tasklist(businessUnitId, name):
+    global tasklists_generated
+
+    r = requests.post(f'{ENDPOINT}/tasklist', data = {
+        "businessUnitId": businessUnitId,	
+        "name": name,
+    }, verify=False, headers={'Authorization': f'Bearer {ADMIN_KEY}'})
+    if r.status_code == 200:
+        tasklists_generated += 1
+    else:
+        print('Hmm, we got an error creating a tasklist', r.text)
+
+    return r.json()
+
+def create_task(taskListId, name, sequence_number):
+    global tasks_generated
+
+    r = requests.post(f'{ENDPOINT}/task', data = {
+        "taskListId": taskListId,	
+        "name": name,
+        "sequenceNumber": sequence_number,
+    }, verify=False, headers={'Authorization': f'Bearer {ADMIN_KEY}'})
+    if r.status_code == 200:
+        tasks_generated += 1
+    else:
+        print('Hmm, we got an error creating a task', r.text)
 
     return r.json()
 
@@ -162,6 +204,7 @@ maul_employee_id = create_employee(maul['id'], sith_blue_milk_cafe['id'], 'SP26'
 register_terror = create_position(sith_blue_milk_cafe['id'], "Register Terror", 10.00)
 darth_barista = create_position(sith_blue_milk_cafe['id'], "Darth Barista", 12.00)
 electric_back_bar = create_position(sith_blue_milk_cafe['id'], "Electric Back Bar", 10.00)
+create_tasklist(sith_blue_milk_cafe['id'], "Clean Up")
 
 
 jedi_fitness_center = create_business_unit("Jedi Fitness Center")
@@ -169,14 +212,27 @@ create_employee(obi_wan['id'], jedi_fitness_center['id'], 'SP26', True, 40, 0, T
 create_employee(jabba['id'], jedi_fitness_center['id'], 'SP26', True, 40, 0, False)
 create_employee(ahsoka['id'], jedi_fitness_center['id'], 'SP26', True, 32, 0, False)
 create_employee(anakin['id'], jedi_fitness_center['id'], 'SP26', False, 40, 0, False)
-create_position(jedi_fitness_center['id'], "Gatekeeper", 10.00)
-create_position(jedi_fitness_center['id'], "Master of Physical Forms", 10.00)
-create_position(jedi_fitness_center['id'], "Force Conditioning Specialist", 12.00)
+gate_keeper = create_position(jedi_fitness_center['id'], "Gatekeeper", 10.00)
+physical_form_coach = create_position(jedi_fitness_center['id'], "Master of Physical Forms", 10.00)
+conditioning_specialist = create_position(jedi_fitness_center['id'], "Force Conditioning Specialist", 12.00)
+wipe_equipment = create_tasklist(jedi_fitness_center['id'], "Wipe Down Equipment")
+create_task(wipe_equipment['id'], 'Wipe down force weights (1)', 1)
+create_task(wipe_equipment['id'], 'Wipe down holo bench (3)', 3)
+create_task(wipe_equipment['id'], 'Wipe down saber trainer (2)', 2)
 
 # Shifts:
 TODAYS_DATE = str(datetime.today())[0:10]
 create_shift(darth_vader_employee_id['id'], sith_blue_milk_cafe['id'], darth_barista['id'], "8:00", "13:00", TODAYS_DATE, True)
-# create_shift(darth_vader['id'], sith_blue_milk_cafe['id'], darth_barista['id'], "8:00", "13:00", TODAYS_DATE, True)
+create_shift(darth_vader_employee_id['id'], sith_blue_milk_cafe['id'], electric_back_bar['id'], "8:00", "13:00", TODAYS_DATE, True)
+
+
+if (YOUR_EMAIL):
+    # add your user to some of these in order to have good dummy data for easy FE testing.
+    your_user = get_existing_user(YOUR_EMAIL);
+    your_users_employee = create_employee(your_user['id'], jedi_fitness_center['id'], 'SP26', True, 40, 0, True) # you work at the fitness center
+    create_shift(your_users_employee['id'], jedi_fitness_center['id'], gate_keeper['id'], "8:00", "13:00", TODAYS_DATE, True)
+    create_shift(your_users_employee['id'], jedi_fitness_center['id'], physical_form_coach['id'], "15:00", "17:00", TODAYS_DATE, True)
+    create_shift(your_users_employee['id'], jedi_fitness_center['id'], conditioning_specialist['id'], "22:00", "23:59", TODAYS_DATE, True)
 
 
 
@@ -186,3 +242,5 @@ print("Generated",business_units_generated,"Business Units.")
 print("Generated",employees_generated,"Employees.")
 print("Generated",positions_created,"Positions.")
 print("Generated",shifts_generated,"Shifts.")
+print("Generated",tasklists_generated,"Tasklists.")
+print("Generated",tasks_generated,"Tasks.")
