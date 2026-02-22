f2dea766948810b15a9f3aeb8ff3b0041297a683
JohnEveryOC <j.every@eagles.oc.edu>
2026-02-22 03:33:03 -0600

    add task post creation taskcompletions

--- app/controllers/task.controller.ts
+++ app/controllers/task.controller.ts
@@ -1,12 +1,33 @@
 import db from "../models/index.ts";
 const Task = db.Task;
+const TaskList = db.TaskList;
 import pkg from 'express';
 import { AppError } from "../error/app.error.ts";
 import { getOneForId } from "../services/services.ts";
+import { Model } from "sequelize";
+import TaskCompletion from "../models/taskcompletion.model.ts";
 
 const exports: any = {};
 const errorClassName = "Task";
 
+exports.create = async (req: pkg.Request, res: pkg.Response) => {
+    req.body.id = undefined;
+    const task = await Task.create(req.body);
+    const taskId = task.id;
+
+    const taskList = await TaskList.findOne({ where: { id: task.taskListId } });
+    //despite the name, it returns multiple. getShifts doesnt exist.
+    const shifts: Model<any, any>[] = await taskList!.getShift();
+    for (let shift of shifts) {
+        const taskCompletion = {
+            "checkedOff": "false",
+            "taskId": taskId,
+            "shiftId": shift.dataValues.id
+        }
+        await TaskCompletion.create(taskCompletion);
+    }
+    res.send(task);
+}
 
 exports.update = async (req: pkg.Request, res: pkg.Response) => {
     const id = parseInt(req.params.id, 10);
