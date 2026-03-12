c5c50d2fad5f312fc6389d198f9a4fbfef749c2d
Emily Forster <emily.forster@eagles.oc.edu>
2026-03-12 17:13:49 -0500

    add: settings model + settings enum file

---
+++ app/models/settings.model.ts
@@ -0,0 +1,39 @@
+import Sequelize from "sequelize";
+import SequelizeInstance from "../config/sequelizeInstance.ts";
+import { settingTypes } from "../types/settings.enum.ts";
+
+//settings
+const Settings = SequelizeInstance.define("settings", {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    description: {
+        type: Sequelize.STRING,
+        allowNull: true,
+    },
+    code: {
+        type: Sequelize.STRING,
+        allowNull: true,
+    },
+    type: {
+        type: Sequelize.ENUM(...Object.values(settingTypes)),
+    },
+    intMin: {
+        type: Sequelize.INTEGER
+    },
+    intMax: {
+        type: Sequelize.INTEGER
+    },
+    forBusiness: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+    },
+});
+
+export default Settings;
