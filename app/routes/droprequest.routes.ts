96fdedb28a17cb87163a8cab81ab4c41dfb7fb41
JohnEveryOC <j.every@eagles.oc.edu>
2026-03-25 15:26:09 -0500

    fix drop request routes

--- app/routes/droprequest.routes.ts
+++ app/routes/droprequest.routes.ts
@@ -1,27 +1,27 @@
 import auth from "../authorization/authorization.ts";
 import generalcontroller from "../controllers/general.controller.ts"
-import CoverRequestModel from "../models/coverrequest.model.ts";
-import coverrequests from "../controllers/coverrequest.controller.ts"
+import DropRequestModel from "../models/droprequest.model.ts";
+import droprequests from "../controllers/droprequest.controller.ts"
 import { Router } from "express";
 var router = Router()
 
 
 // Create a new Position
-router.post("/", [auth.authenticate], generalcontroller.create(CoverRequestModel));
+router.post("/", [auth.authenticate], generalcontroller.create(DropRequestModel));
 
 // Retrieve all Positions
-router.get("/all", [auth.authenticate], coverrequests.findAll);
+router.get("/all", [auth.authenticate], droprequests.findAll);
 
 // Retrieve a single Position by id
-router.get("/:id", [auth.authenticate], coverrequests.findOne);
+router.get("/:id", [auth.authenticate], droprequests.findOne);
 
 // Update a Position by id
-router.put("/:id", [auth.authenticate], coverrequests.update);
+router.put("/:id", [auth.authenticate], droprequests.update);
 
 // Delete a Position by id
-router.delete("/:id", [auth.authenticate], generalcontroller.delete(CoverRequestModel));
+router.delete("/:id", [auth.authenticate], generalcontroller.delete(DropRequestModel));
 
-router.put("/:id/approve/:approverId", [auth.authenticate], coverrequests.approveCoverRequest);
+router.put("/:id/approve/:approverId", [auth.authenticate], droprequests.approveDropRequest);
 
 
 export default router;
