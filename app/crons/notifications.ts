6c0f0ad87671853384f30caed1f87688c31e10ea
Lance Skinner <sirlancelot2604@outlook.com>
2026-03-27 14:51:59 -0500

    Add timezone fix

--- app/crons/notifications.ts
+++ app/crons/notifications.ts
@@ -2,13 +2,14 @@ import cron from 'node-cron';
 import Shift from '../models/shift.model.ts';
 import { Op } from 'sequelize';
 import moment from 'moment';
+import 'moment-timezone';
 import { sendNotificationToEmployee } from '../services/notifications.ts';
 
 
 // every 5 minutes
 // note, behavior is undefined during daylight savings times
 cron.schedule("*/5 * * * *", async () => {
-    const momentInOneHour = moment().add(1, 'hours');
+    const momentInOneHour = moment().tz("America/Chicago").add(1, 'hours');
     const date = momentInOneHour.toDate().toLocaleDateString('en-CA', { timeZone: 'America/Chicago' });
     const time = momentInOneHour.toDate().toLocaleTimeString("en-US", { hour12: false }).substring(0, 5)+":00";
     console.log(date)
