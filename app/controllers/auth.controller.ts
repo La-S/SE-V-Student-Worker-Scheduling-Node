0aca0eee71ad9ab52b869c05e2f7b0a5be141dc1
Lance Skinner <sirlancelot2604@outlook.com>
2026-04-13 16:10:18 -0500

    improve token life

--- app/controllers/auth.controller.ts
+++ app/controllers/auth.controller.ts
@@ -68,7 +68,7 @@ exports.login = async (req: pkg.Request, res: pkg.Response) => {
   if (!sessionToken) {
     // create a new Session with an expiration date and save to database
     let token = jwt.sign({ id: googleUserInfo.email }, authconfig.secret, {
-      expiresIn: 86400,
+      expiresIn: 3600 * 24 * 31, // expires once every 31 days.
     });
     let tempExpirationDate = new Date();
     tempExpirationDate.setDate(tempExpirationDate.getDate() + 1);
