21f640cc7705cc0426ef644ce4782fc7a0aa00eb
Gusify <corderomgus@gmail.com>
2026-04-16 23:16:22 -0500

    readd preexisting validation checking - removed by mistake

--- app/controllers/announcement.controller.ts
+++ app/controllers/announcement.controller.ts
@@ -14,9 +14,11 @@ import { sendNotificationToEmployee } from '../services/notifications.ts';
 import {
     sendAnnouncementEmailToEmployeeIds,
 } from '../services/mailer.ts';
+import BusinessUnit from '../models/businessunit.model.ts';
 
 exports.create = async (req: pkg.Request, res: pkg.Response) => {
     let sendNotifNow = false;
+    await getOneForId(BusinessUnit, req.body.businessUnitId);
     const employees = await Employee.findAll({
         where: {
             businessUnitId: req.body.businessUnitId,
@@ -73,7 +75,7 @@ exports.create = async (req: pkg.Request, res: pkg.Response) => {
 exports.createSpecificEmployees = async (req: pkg.Request, res: pkg.Response) => {
     let sendNotifNow = false;
     const employeeIds: number[] = req.body.employeeIds;
-
+    await getOneForId(BusinessUnit, req.body.businessUnitId);
     const today = new Date().toLocaleDateString('en-CA', { timeZone: 'America/Chicago' });
     const currentTime = new Date().toLocaleTimeString("en-US", { timeZone: 'America/Chicago', hour12: false });
 
