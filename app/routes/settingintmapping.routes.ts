37b0b4f2c84dff44086781b42bc1a5aa447b0001
JohnEveryOC <j.every@eagles.oc.edu>
2026-04-20 14:36:33 -0500

    comment out settingintmapping routes

--- app/routes/settingintmapping.routes.ts
+++ app/routes/settingintmapping.routes.ts
@@ -5,9 +5,10 @@ import SettingIntMappingModel from "../models/settingintmapping.model.ts"
 import { Router } from "express";
 var router = Router()
 
+//PLEASE USE SETTINGS POST/PUT TO UPDATE SETTING INT MAPPINGS
 
-// Create a new SettingIntMappingSettingValue
-router.post("/", [authenticate, isAdminOnly], settingIntMappings.create);
+// // Create a new SettingIntMappingSettingValue
+// router.post("/", [authenticate, isAdminOnly], settingIntMappings.create);
 
 // Retrieve all SettingIntMappingSettingValues
 router.get("/all", [authenticate, isAdminOnly], generalcontroller.findAll(SettingIntMappingModel));
@@ -15,10 +16,10 @@ router.get("/all", [authenticate, isAdminOnly], generalcontroller.findAll(Settin
 // Retrieve a single SettingIntMappingSettingValue by id
 router.get("/:id", [authenticate], settingIntMappings.findOne);
 
-// Update a SettingIntMappingSettingValue by id
-router.put("/:id", [authenticate, isAdminOnly], settingIntMappings.update);
+// // Update a SettingIntMappingSettingValue by id
+// router.put("/:id", [authenticate, isAdminOnly], settingIntMappings.update);
 
-// Delete a SettingIntMappingSettingValue by id
-router.delete("/:id", [authenticate, isAdminOnly], settingIntMappings.delete);
+// // Delete a SettingIntMappingSettingValue by id
+// router.delete("/:id", [authenticate, isAdminOnly], settingIntMappings.delete);
 
 export default router;
