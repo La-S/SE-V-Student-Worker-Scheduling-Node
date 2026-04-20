8aad910bc82c22b9e011a9b82644a1e440dda07e
Lance Skinner <sirlancelot2604@outlook.com>
2026-04-20 14:20:43 -0500

    add security for usersettingvalue

--- app/routes/usersettingvalue.routes.ts
+++ app/routes/usersettingvalue.routes.ts
@@ -14,10 +14,10 @@ router.post("/", [authenticate, isAdminOnly], userSettingValues.create);
 router.get("/all", [authenticate, isAdminOnly], generalcontroller.findAll(UserSettingValueModel));
 
 // Retrieve a single UserSettingValue by id
-router.get("/:id", [authenticate, authorizeById("user")], userSettingValues.findOne);
+router.get("/:id", [authenticate, authorizeById("userSettingsValue")], userSettingValues.findOne);
 
 // Update a UserSettingValue by id
-router.put("/:id", [authenticate, authorizeById("user")], userSettingValues.update);
+router.put("/:id", [authenticate, authorizeById("userSettingsValue")], userSettingValues.update);
 
 // Delete a UserSettingValue by id
 router.delete("/:id", [authenticate, isAdminOnly], generalcontroller.delete(UserSettingValueModel));
