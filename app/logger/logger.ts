43db742920a8b292da238abdf60a48a485f2b9bb
Lance Skinner <sirlancelot2604@outlook.com>
2026-04-20 11:10:49 -0500

    whoops, add timestamp too

--- app/logger/logger.ts
+++ app/logger/logger.ts
@@ -3,6 +3,7 @@ import winston from 'winston';
 export const logger = winston.createLogger({
   level: 'info',
   format: winston.format.combine(
+    winston.format.timestamp(),
     winston.format.errors({stack: true}),
     winston.format.json(),
   ),
