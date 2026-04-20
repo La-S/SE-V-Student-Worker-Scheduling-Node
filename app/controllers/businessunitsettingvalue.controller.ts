0f8075ae7e7df75143fd008320666e154d7ab2b1
Lance Skinner <sirlancelot2604@outlook.com>
2026-04-20 14:31:21 -0500

    remove console log

--- app/controllers/businessunitsettingvalue.controller.ts
+++ app/controllers/businessunitsettingvalue.controller.ts
@@ -36,7 +36,6 @@ exports.update = async (req: pkg.Request, res: pkg.Response) => {
     const setting = await getOneForStringId(Setting, businessUnitSettingValue.dataValues.settingCode);
 
     if (setting.dataValues.type === "string" && typeof req.body.settingValue == "string"){
-        console.log("test");
         const settingIntMapping = await SettingIntMapping.findOne({where: {settingCode: setting.dataValues.code, stringValue: req.body.settingValue}});
         if (!settingIntMapping){
             throw new AppError(400, `Setting mapping for ${req.body.settingValue} not found`);
